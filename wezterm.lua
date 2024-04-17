02e883f54387b2d5fbc77b8d469b24d29fb17f4b
Tung Pham <joseph.pham280996@gmail.com>
2024-04-16 23:50:10 -0400

    Update for platform specific setting

--- wezterm.lua
+++ wezterm.lua
@@ -4,21 +4,23 @@ local config = wezterm.config_builder()
 
 -- Fonts and Colors
 local options = {
-  color_scheme = 'Kanagawa (Gogh)',
+	color_scheme = "Kanagawa (Gogh)",
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 9,
 	hide_tab_bar_if_only_one_tab = true,
-
-	default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" },
 	launch_menu = {
 		{
 			label = "Bash",
 			args = { "bash" },
 		},
 		{ label = "Htop", args = { "htop" } },
-    { label = "Python", args = { "python" }}
+		{ label = "Python", args = { "python" } },
 	},
 }
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	options["default_prog"] = { "C:/Program Files/PowerShell/7/pwsh.exe" }
+end
 
 for setting, value in pairs(options) do
 	config[setting] = value
