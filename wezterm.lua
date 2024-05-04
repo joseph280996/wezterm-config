e31959d1026c91ec5539c73f08baa07862857e1f
Tung Pham <joseph.pham280996@gmail.com>
2024-05-03 21:26:16 -0400

    Remove some functionality since it's already done in zellij

--- wezterm.lua
+++ wezterm.lua
@@ -3,13 +3,14 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 -- Fonts and Colors
 local options = {
-  -- debug_key_events = true,
+	-- debug_key_events = true,
 	color_scheme = "Kanagawa (Gogh)",
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 10,
 	hide_tab_bar_if_only_one_tab = true,
-  disable_default_key_bindings = true,
+	disable_default_key_bindings = true,
 	launch_menu = {
+		{ label = "Pwsh 7", args = { "pwsh" } },
 		{
 			label = "Bash",
 			args = { "bash" },
@@ -20,10 +21,14 @@ local options = {
 		{ label = "Node", args = { "node" } },
 	},
 	keys = require("keymaps"),
+	default_cwd = "~",
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	options["default_prog"] = { "C:/Program Files/PowerShell/7/pwsh.exe" }
+	config.wsl_domains = {
+		{ name = "WSL:Arch", distribution = "Arch", default_cwd = "~" },
+	}
+	config.default_domain = "WSL:Arch"
 end
 
 for setting, value in pairs(options) do
