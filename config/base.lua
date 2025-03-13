b9aab9539a94098ae7132cc98cf0b38d3be8d530
Tung Pham <joseph.pham280996@gmail.com>
2025-03-13 00:57:18 -0400

    chore: clean up settings that is set by default

--- config/base.lua
+++ config/base.lua
@@ -3,8 +3,6 @@ return {
 	-- debug_key_events = true,
 	color_scheme = "Kanagawa (Gogh)",
 	font = wezterm.font("JetBrains Mono"),
-	disable_default_key_bindings = true,
-  disable_default_mouse_bindings = true,
 	launch_menu = {
 		{ label = "Python", args = { "python" } },
 		{ label = "Node", args = { "node" } },
