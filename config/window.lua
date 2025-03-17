9b24bea3df6c69f152040fb1e64ba3bcfc9f0f0d
Tung Pham <joseph.pham280996@gmail.com>
2025-03-17 00:21:10 -0400

    feat(config): Re-ordering environments on new tab prompt

--- config/window.lua
+++ config/window.lua
@@ -6,15 +6,17 @@ return {
 	hide_tab_bar_if_only_one_tab = false,
 	launch_menu = {
 		{
-			label = "Default Arch WSL",
-			domain = { DomainName = "WSL-Arch" },
-		},
-		{ label = "Pwsh 7", args = { "pwsh" }, domain = { DomainName = "local" } },
+			label = "Bash (local)",
 		{
 			label = "Bash",
 			args = { "bash" },
 			domain = { DomainName = "local" },
 		},
+		{
+			label = "Fish (Arch WSL)",
+			domain = { DomainName = "WSL-Arch" },
+		},
+		{ label = "Pwsh 7 (local)", args = { "pwsh" }, domain = { DomainName = "local" } },
 	},
 	default_prog = { "C:/Program Files/Git/bin/bash" },
 	font_size = 7.5,
@@ -23,5 +25,4 @@ return {
 	},
 	keys = km,
 	default_workspace = "default",
-	leader = { key = "?", mods = "CTRL|SHIFT", timeout_milliseconds = 1000 },
 }
