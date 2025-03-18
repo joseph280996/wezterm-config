4543881a6fbbb7972677ce3d787e15f338c71a85
Tung Pham <joseph.pham280996@gmail.com>
2025-03-18 18:48:05 -0400

    style(config/window): use DefaultDomain instead of local domain name

--- config/window.lua
+++ config/window.lua
@@ -3,18 +3,21 @@ local km = require("keymaps.base")
 utils.tbl_extend(require("keymaps.base"), require("keymaps.window"))
 
 return {
-	hide_tab_bar_if_only_one_tab = false,
 	launch_menu = {
 		{
 			label = "Bash (local)",
 			args = { "bash" },
-			domain = { DomainName = "local" },
+			domain = "DefaultDomain",
+		},
+		{
+			label = "Pwsh 7 (local)",
+			args = { "pwsh" },
+			domain = "DefaultDomain",
 		},
 		{
 			label = "Fish (Arch WSL)",
 			domain = { DomainName = "WSL-Arch" },
 		},
-		{ label = "Pwsh 7 (local)", args = { "pwsh" }, domain = { DomainName = "local" } },
 	},
 	default_prog = { "C:/Program Files/Git/bin/bash" },
 	font_size = 7.5,
