685bfd7498e79f20bdf806374afbb5c9afe6c4ef
Tung Pham <joseph.pham280996@gmail.com>
2025-03-20 17:33:28 -0400

    fix(config/mac): failed to import utils error

--- config/mac.lua
+++ config/mac.lua
@@ -1,4 +1,4 @@
-local utils = require('utils')
+local utils = require('utils.tables')
 local km = require("keymaps.base")
 
 utils.tbl_extend(km, require("keymaps.mac"))
@@ -6,5 +6,4 @@ utils.tbl_extend(km, require("keymaps.mac"))
 return {
 	font_size = 12,
 	keys = km,
-	hide_tab_bar_if_only_one_tab = false,
 }
