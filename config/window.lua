10a7537e38c220da045f7a18b11583919cf03849
Tung Pham <joseph.pham280996@gmail.com>
2025-06-28 22:47:19 -0400

    style: Fix config/window.lua formatting

--- config/window.lua
+++ config/window.lua
@@ -3,27 +3,27 @@ local km = require("keymaps.base")
 utils.tbl_extend(require("keymaps.base"), require("keymaps.window"))
 
 return {
-	launch_menu = {
-		{
-			label = "Bash (local)",
-			args = { "bash" },
-			domain = "DefaultDomain",
-		},
-		{
-			label = "Pwsh 7 (local)",
-			args = { "pwsh" },
-			domain = "DefaultDomain",
-		},
-		{
-			label = "Fish (Arch WSL)",
-			domain = { DomainName = "WSL-Arch" },
-		},
-	},
-	default_prog = { "C:/Program Files/Git/bin/bash" },
-	font_size = 7.5,
-	wsl_domains = {
-		{ name = "WSL-Arch", distribution = "Arch", default_cwd = "~" },
-	},
-	keys = km,
-	default_workspace = "default",
+  launch_menu = {
+    {
+      label = "Bash (local)",
+      args = { "bash" },
+      domain = "DefaultDomain",
+    },
+    {
+      label = "Pwsh 7 (local)",
+      args = { "pwsh" },
+      domain = "DefaultDomain",
+    },
+    {
+      label = "Fish (Arch WSL)",
+      domain = { DomainName = "WSL-Arch" },
+    },
+  },
+  default_prog = { "C:/Program Files/Git/bin/bash" },
+  font_size = 7.5,
+  wsl_domains = {
+    { name = "WSL-Arch", distribution = "Arch", default_cwd = "~" },
+  },
+  keys = km,
+  default_workspace = "default",
 }
