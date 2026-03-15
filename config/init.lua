0f44abfaa3b20b5429aa56c6655990570c72d7fa
Tung Pham <joseph.pham280996@gmail.com>
2026-03-15 15:56:24 -0500

    feat: Add more location for quick access

--- config/init.lua
+++ config/init.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local km = require("config.keymaps")
 local tabline = require("config.tabline")
-local tbl_utils = require('utils.tables')
+local tbl_utils = require("utils.tables")
 
 local M = {
 	disable_default_key_bindings = true,
@@ -11,26 +11,55 @@ local M = {
 	launch_menu = {
 		{
 			label = "Knowledge Base",
-			cwd = "/Users/tpham4/Documents/knowledge-base/",
+			cwd = wezterm.home_dir .. "/Documents/knowledge-base/",
 		},
 		{
-			label = "JIRA Notes",
-			cwd = "/Users/tpham4/Documents/notes/",
+			label = "Notes",
+			cwd = wezterm.home_dir .. "/Documents/notes/",
 		},
 		{
 			label = "Neovim Config",
-			cwd = "/Users/tpham4/.config/nvim",
+			cwd = wezterm.home_dir .. "/.config/nvim",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim" },
 		},
 		{
 			label = "Wezterm Config",
-			cwd = "/Users/tpham4/.config/wezterm",
+			cwd = wezterm.home_dir .. "/.config/wezterm",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim" },
 		},
-		{ label = "Python", args = { "python" } },
-		{ label = "Node", args = { "node" } },
+		{
+			label = "Fish Config",
+			cwd = wezterm.home_dir .. "/.config/fish",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim" },
+		},
+		{
+			label = "Claude Desktop Config",
+			cwd = wezterm.home_dir .. "/Library/Application Support/Claude/",
+			args = {
+				"/opt/homebrew/bin/fish",
+				"-l",
+				"-c",
+				"nvim " .. wezterm.home_dir .. "/claude_desktop_config.json",
+			},
+		},
+		{
+			label = "Claude Code Config",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim " .. wezterm.home_dir .. "/.claude.json" },
+		},
+		{
+			label = "Aerospace Config",
+			cwd = wezterm.home_dir .. "/.config/aerospace",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim" },
+		},
+		{
+			label = "OpenCode Config",
+			cwd = wezterm.home_dir .. "/.config/opencode",
+			args = { "/opt/homebrew/bin/fish", "-l", "-c", "nvim" },
+		},
 	},
 	-- debug_key_events = true,
 	color_scheme = "Kanagawa (Gogh)",
-	default_cwd = "$HOME",
+	default_cwd = wezterm.home_dir,
 	leader = { key = "/", mods = "CTRL", timeout_milliseconds = 1000 },
 }
 
